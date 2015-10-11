272e8d306efec7bd0e6232705b849fb33e7a8f15
Ronert Obst <ronert.obst@gmail.com>
2015-10-11 17:57:19 +0200

    Initial Commit

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://ronert-obst.com'
+SITEURL = 'http://everything-counts.cfapps.io'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
