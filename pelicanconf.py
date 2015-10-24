ec614591cdb20b9df0d31eb1b40d6d4af41d2b96
Ronert Obst <ronert.obst@gmail.com>
2015-10-24 10:29:56 +0200

    add static paths

--- pelicanconf.py
+++ pelicanconf.py
@@ -12,6 +12,8 @@ TIMEZONE = 'Europe/Berlin'
 
 DEFAULT_LANG = 'en'
 
+STATIC_PATHS = ['images', 'notebooks', 'code', 'downloads', 'figures', 'pdfs']
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
